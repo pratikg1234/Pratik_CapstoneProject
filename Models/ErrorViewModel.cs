8faaa79ac3470bc95dd42ea5fd14ca16d731b08f
ram <dev2@gmail.com>
2024-11-19 11:33:09 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pratik_mvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
